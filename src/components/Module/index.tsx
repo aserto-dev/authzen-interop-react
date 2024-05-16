ecb7878985ac187df451f818158a3e5bafe0a83c
Omri Gazitt <ogazitt@gmail.com>
2024-05-15 20:34:02 -0700

    updated policy modules to match new policy-todo-authzen

--- src/components/Module/index.tsx
+++ src/components/Module/index.tsx
@@ -4,15 +4,15 @@ import Highlight from '../Highlight'
 
 const modules: Record<string, string> = {
   'GET /users/:userId': `
-package todoApp.GET.users.__userID
+package todoApp.can_read_user
 
 default allowed = true`,
   'GET /todos': `
-package todoApp.GET.todos
+package todoApp.can_read_todos
 
 default allowed = true`,
   'POST /todos': `
-package todoApp.POST.todos
+package todoApp.can_create_todo
 
 import future.keywords.in
 import input.user
@@ -25,9 +25,8 @@ allowed {
   user.properties.roles[_] == x
 }`,
   'PUT /todos/:id': `
-package todoApp.PUT.todos.__id
+package todoApp.can_update_todo
 
-import future.keywords.in
 import input.resource
 import input.user
 
@@ -42,9 +41,8 @@ allowed {
   user.properties.roles[_] == "evil_genius"
 }`,
   'DELETE /todos/:id': `
-package todoApp.DELETE.todos.__id
+package todoApp.can_delete_todo
 
-import future.keywords.in
 import input.resource
 import input.user
 
